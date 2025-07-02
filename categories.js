b121c1f1e5f19a714af9ee27c62cb759ce580b9e
selim2066 <121637755+selim2066@users.noreply.github.com>
2025-07-02 10:18:45 +0600

    all button clicked

--- categories.js
+++ categories.js
@@ -216,24 +216,14 @@ modalContent.innerHTML=`
 
 
 
-  //         <!-- Open the modal using ID.showModal() method -->
-  // <button class="btn" onclick="my_modal_5.showModal()">open modal</button>
-  // <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
-  //   <div class="modal-box">
-  //     <h3 class="text-lg font-bold">Hello!</h3>
-  //     <p class="py-4">Press ESC key or click the button below to close</p>
-  //     <div class="modal-action">
-  //       <form method="dialog">
-  //         <!-- if there is a button in form, it will close the modal -->
-  //         <button class="btn">Close</button>
-  //       </form>
-  //     </div>
-  //   </div>
-  // </dialog>
 }
 
 document.getElementById('searchInput').addEventListener('keyup', (e)=>{
-loadVideos(e.target.value)
+  loadVideos(e.target.value)
+})
+
+document.getElementById('all').addEventListener('click',function(){
+  loadVideos()
 })
 
 loadCategories();
@@ -241,9 +231,9 @@ loadVideos();
 
 // button
 /**<div class="card-actions justify-end">
-     <button class="btn btn-neutral btn-dash hover:bg-red-700 hover:border-none text-xl w-34 h-16">Watch</button>
-     </div> */
-// const btn = document.createElement('button')
+ <button class="btn btn-neutral btn-dash hover:bg-red-700 hover:border-none text-xl w-34 h-16">Watch</button>
+ </div> */
+ // const btn = document.createElement('button')
 // btn.classList.add('btn', 'w-35', 'h-20', 'text-3xl', 'btn-neutral', 'btn-dash')
 // btn.innerText = element.category
 
@@ -251,35 +241,50 @@ loadVideos();
 // https://openapi.programming-hero.com/api/phero-tube/videos
 // console.log(element)
 /*
+
+updated:
+<div class="card bg-base-100 shadow-sm sm:w-[550px]  lg:w-full">
+<figure>
+<img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+</figure>
+<div class="card-body">
+<h2 class="card-title font-bold text-3xl">Card Title</h2>
+<p class="text-xl">A card component has a figure, a body part, and inside body there are title and actions parts</p>
+<div class="card-actions justify-end">
+<button class="btn btn-primary text-xl w-34 h-16">Buy Now</button>
+</div>
+</div>
      
-     updated:
-     <div class="card bg-base-100 shadow-sm sm:w-[550px]  lg:w-full">
-     <figure>
-     <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-     </figure>
-     <div class="card-body">
-     <h2 class="card-title font-bold text-3xl">Card Title</h2>
-     <p class="text-xl">A card component has a figure, a body part, and inside body there are title and actions parts</p>
-     <div class="card-actions justify-end">
-     <button class="btn btn-primary text-xl w-34 h-16">Buy Now</button>
-     </div>
-     </div>
-     
-     </div>
-     
-     
-     
-     
-     <div class="card bg-base-100 shadow-sm">
-     <figure>
-     <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-     </figure>
-     <div class="card-body">
-     <h2 class="card-title font-bold">Card Title</h2>
-     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+</div>
+
+
+
+
+<div class="card bg-base-100 shadow-sm">
+<figure>
+<img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+</figure>
+<div class="card-body">
+<h2 class="card-title font-bold">Card Title</h2>
+<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
      <div class="card-actions justify-end">
      <button class="btn btn-primary">Buy Now</button>
      </div>
      </div>
      </div>
      */
+    
+    //         <!-- Open the modal using ID.showModal() method -->
+    // <button class="btn" onclick="my_modal_5.showModal()">open modal</button>
+    // <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+    //   <div class="modal-box">
+    //     <h3 class="text-lg font-bold">Hello!</h3>
+    //     <p class="py-4">Press ESC key or click the button below to close</p>
+    //     <div class="modal-action">
+    //       <form method="dialog">
+    //         <!-- if there is a button in form, it will close the modal -->
+    //         <button class="btn">Close</button>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </dialog>
